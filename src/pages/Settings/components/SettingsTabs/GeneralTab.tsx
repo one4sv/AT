8e31d5b6ae50c582aed9ad1c6f261ccc8f021da7
one4sv <kutenko18@gmail.com>
2026-08-10 11:21:16 +0300

    fix

--- src/pages/Settings/components/SettingsTabs/GeneralTab.tsx
+++ src/pages/Settings/components/SettingsTabs/GeneralTab.tsx
@@ -3,78 +3,82 @@ import RadioGroup from "../../../../components/ts/RadioGroup";
 import SeekBar from "../SeekBar";
 import SelectList from "../../../../components/ts/SelectList";
 import { useTranslation } from "react-i18next";
+import { useSettings } from "../../../../components/hooks/SettingsHook";
+import { useUpSettings } from "../../../../components/hooks/UpdateSettingsHook";
 
 export default function GeneralTab() {
     const { t } = useTranslation("settings");
-    const [fontSize, setFontSize] = useState<number>(16);
+    const { lang } = useSettings()
+    const { setNewLang } = useUpSettings()
+    const [ fontSize, setFontSize ] = useState<number>(16);
 
     const timeChanger = [
-        { label: t("genearal.twentyFour"), value: "twentyFour" },
-        { label: t("genearal.twelve"), value: "twelve" },
+        { label: t("general.twentyFour"), value: "twentyFour" },
+        { label: t("general.twelve"), value: "twelve" },
     ];
     const dateChanger = [
-        { label: t("genearal.dateStandard"), value: "standart" },
-        { label: t("genearal.dateAmerican"), value: "american" },
+        { label: t("general.dateStandard"), value: "standart" },
+        { label: t("general.dateAmerican"), value: "american" },
     ];
     const trafficEconomy = [
-        { label: t("genearal.on"), value: "on" },
-        { label: t("genearal.off"), value: "off" },
+        { label: t("general.on"), value: "on" },
+        { label: t("general.off"), value: "off" },
     ];
     const langArr = [
-        { label: t("genearal.russian"), value: "rus" },
-        { label: t("genearal.english"), value: "eng" },
+        { label: t("general.russian"), value: "ru" },
+        { label: t("general.english"), value: "en" },
     ];
 
     return (
         <div className="settingTab">
             <div className="settingInnerDiv">
                 <div className="settingHeader">
-                    {t("genearal.language")}
+                    {t("general.language")}
                 </div>
-                <SelectList arr={langArr} className="settingSL" selected={"rus"} />
+                <SelectList arr={langArr} className="settingSL" selected={lang} extraFunction={setNewLang}/>
             </div>
             <div className="settingInnerDiv">
                 <div className="settingHeader">
-                    {t("genearal.traffic")}
+                    {t("general.traffic")}
                 </div>
                 <div className="settingInnerWrapper">
                     <div className="settingSpan">
-                        {t("genearal.autoLoadImages")}
+                        {t("general.autoLoadImages")}
                     </div>
                     <RadioGroup list={trafficEconomy} val="on" />
                 </div>
                 <div className="settingInnerWrapper">
                     <div className="settingSpan">
-                        {t("genearal.autoPlayGif")}
+                        {t("general.autoPlayGif")}
                     </div>
                     <RadioGroup list={trafficEconomy} val="on" />
                 </div>
                 <div className="settingInnerWrapper">
                     <div className="settingSpan">
-                        {t("genearal.useImageCache")}
+                        {t("general.useImageCache")}
                     </div>
                     <RadioGroup list={trafficEconomy} val="on" />
                 </div>
             </div>
             <div className="settingInnerDiv">
                 <div className="settingHeader">
-                    {t("genearal.text")}
+                    {t("general.text")}
                 </div>
                 <div className="settingInnerWrapper">
                     <div className="settingSpan">
-                        {t("genearal.fontSize")}
+                        {t("general.fontSize")}
                     </div>
                     <SeekBar min={12} max={22} value={fontSize} unit="px" step={1} onChange={setFontSize} />
                 </div>
                 <div className="settingInnerWrapper">
                     <div className="settingSpan">
-                        {t("genearal.dateFormat")}
+                        {t("general.dateFormat")}
                     </div>
                     <RadioGroup list={dateChanger} val={"standart"} />
                 </div>
                 <div className="settingInnerWrapper">
                     <div className="settingSpan">
-                        {t("genearal.timeFormat")}
+                        {t("general.timeFormat")}
                     </div>
                     <RadioGroup list={timeChanger} val={"twentyFour"} />
                 </div>
